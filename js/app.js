efe5bef5ea4557f2c9f624615746ba6d7020be40
Marta Kosiorowska <marta.kosiorowska@gmail.com>
2016-04-19 14:42:29 +0200

    do sprawdzenia animacja setIntro w app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
 
+//header - intro
+
+function setIntro() {
+  $(".header_logo").children("strong").each(function(){
+    // $(this).css("color", "white");
+    var dataColor = $(this).data("color");
+    $(this).animate({
+      color: "white"
+    }, 2000, function(){
+      $(this).animate({
+        color: dataColor
+      }, 2000);
+    });
+  });
+}
+
+
+
+function showColor($num){
+  var dataColor = $(".letter_" + $num).data("color");
+  $(".letter_" + $num).animate({ //?
+    color: dataColor
+  }, 200, function(){
+    if($num < 8) {
+      showColor($num+1);
+    }
+    else {
+      setIntro();
+    }
+  });
+}
+
+showColor(1);
+
+// setIntro();
+
 // change cake colour
 
 $(".colours").find("div").each(function(){
@@ -12,72 +48,91 @@ $(".colours").find("div").each(function(){
   });
 });
 
-
-// var isAnimationRunning = false;
+// change number of layers
 
 function addRemoveLayer(){
+
   var i = 0;
+
+  // adding layer
   $(".add_layer_button").on("click", function(){
+
     if ((i<=3)&&(i>0)){
-        var layer = $(".layers_container").children().eq(i-1);
-        var colorBar = $(".colours").children("div").eq(i-1);
+
+      var layer = $(".layers_container").children().eq(i-1);
+      var colorBar = $(".colours").children("div").eq(i-1);
+      var layerHeight = parseInt($(".layer_6").css("height"));
+      var cakeHeight = parseInt(layer.parent().outerHeight());
+      var layerMarginTop = parseInt($(".layer_6").css("margin-top"));
+      var newHeight = cakeHeight + layerHeight + layerMarginTop + "px";
+
+      console.log(i);
+      console.log("plus");
+      console.log(layer);
+      console.log(colorBar);
+
+      colorBar.fadeIn();
+
+      layer.parent().animate({
+        height: newHeight
+      },1000, "easeOutElastic", function(){
+        console.log("complete");
+        layer.slideDown();
+      });
+
+      i--;
+    }
+
+    else {
+      console.log("klops+");
+      console.log(i);
+    }
+  });
+
+  // removing layer
+  $(".remove_layer_button").on("click", function(){
+
+      if ((i>=0)&&(i<3)){
+
+        var layer = $(".layers_container").children().eq(i);
+        var colorBar = $(".colours").children("div").eq(i);
         var layerHeight = parseInt($(".layer_6").css("height"));
-        var cakeHeight = parseInt(layer.parent().outerHeight());
         var layerMarginTop = parseInt($(".layer_6").css("margin-top"));
-        var newHeight = cakeHeight + layerHeight + layerMarginTop + "px";
+
+        var cakeHeight = parseInt(layer.parent().outerHeight());
+        layer.parent().css("height", cakeHeight);
+        var newHeight = cakeHeight - layerHeight - layerMarginTop + "px";
+
         console.log(i);
-        console.log("plus");
+        console.log("minus");
         console.log(layer);
         console.log(colorBar);
-        // layer.show();
-        // layer.parent().css("height", "newHeight");
-        colorBar.show();
+
+        layer.slideUp();
+        colorBar.fadeOut();
+        // var j = 7;
+        // if (j>-1){
+        //   colorBar.children().each(function(j){
+        //     $(this).eq(j).slideUp(500);
+        //   });
+        //   j--;
+        // }
+
+
         layer.parent().animate({
           height: newHeight
-        },1000, "swing", function(){
+        },1000, "easeOutBounce", function(){
           console.log("complete");
-            layer.show();
         });
-        i--;
+
+        i++;
+
       }
-      else {
-        console.log("klops+");
+      else{
+        console.log("klops-");
         console.log(i);
       }
-    });
-  // if(isAnimationRunning === false){
-    $(".remove_layer_button").on("click", function(){
-
-        if ((i>=0)&&(i<3)){
-          // isAnimationRunning = true;
-          var layer = $(".layers_container").children().eq(i);
-          var colorBar = $(".colours").children("div").eq(i);
-          var layerHeight = parseInt($(".layer_6").css("height"));
-          var layerMarginTop = parseInt($(".layer_6").css("margin-top"));
-          var cakeHeight = parseInt(layer.parent().outerHeight());
-          layer.parent().css("height", cakeHeight);
-          var newHeight = cakeHeight - layerHeight - layerMarginTop + "px";
-          console.log(i);
-          console.log("minus");
-          console.log(layer);
-          console.log(colorBar);
-          // layer.hide();
-          layer.hide();
-          colorBar.hide();
-          layer.parent().animate({
-            height: newHeight
-          },1000, "swing", function(){
-            console.log("complete");
-          });
-          i++;
-        }
-        else{
-          console.log("klops-");
-          console.log(i);
-        }
-      });
-    // isAnimationRunning = false;
-  // }
+  });
 }
 
 addRemoveLayer();
@@ -112,8 +167,55 @@ addRemoveLayer();
 //     }
 //   }
 
+//change cake size
+
+function changeSize(){
+  $(".size").each(function(){
+
+    //I take diagonal of this circle
+    var diagonal = parseInt($(this).css("width"));
+    //I take box-shadow
+    var boxShadow = $(this).css('boxShadow')
+
+    //I make an array from this box-shadow
+    var boxShadowArray = (boxShadow).split(' ');
+    console.log("boxShadowArray: " + boxShadowArray);
+    //I get "6" element from this array - blur
+    var boxShadowArray6 = boxShadowArray[6];
+    console.log(boxShadowArray6);
 
 
+      $(this).on("mouseenter", function(){
+
+        //I make new Diagonal
+        var newDiagonal = diagonal*1.02;
+
+        //I modify "6" element
+        var boxShadowArray6Modify = (parseInt(boxShadowArray6)*4 ) + "px";
+        //I push new "6" element to box-shadow array
+        boxShadowArray[6] = boxShadowArray6Modify;
+        //I join to string new box-shadow array
+        var newBoxShadowArray= boxShadowArray.join(' ');
+        console.log(newBoxShadowArray);
+
+        //I push new values to css
+        $(this).css({
+          width: newDiagonal,
+          height: newDiagonal,
+          boxShadow: newBoxShadowArray
+        });
+      });
+
+      $(this).on("mouseleave", function(){
+
+        $(this).css("width", diagonal);
+        $(this).css("height", diagonal);
+        $(this).css("box-shadow", boxShadow);
+      });
+  });
+}
+
+changeSize();
 
 
 });
